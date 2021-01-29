13d9b8190ce05924cf1c2837414f3136048e2ade
galaxyontech <58999064+galaxyontech@users.noreply.github.com>
2021-01-29 21:05:57 +0800

    Update continue_simulation.java

--- src/continue_simulation.java
+++ src/continue_simulation.java
@@ -1,5 +1,8 @@
+import sun.lwawt.macosx.CSystemTray;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class continue_simulation {
@@ -8,6 +11,7 @@ public class continue_simulation {
         String file_name = "Increased_infectors_over_time_step.txt";
         FileWriter myWriter ;
         myWriter = new FileWriter(file_name);
+
 
         // initializing variables
         Person current_student ;
@@ -22,6 +26,13 @@ public class continue_simulation {
         int wallcase = 0;
         int infectors_per_time_step = 0;
         double temp = 0;
+        double aver_cornerDays = 0;
+        double aver_centerDays = 0 ;
+        double aver_wallDays = 0;
+        int academicWeek = 3; // A week has three academic meetings, two class, 1 recitation, the real class situation may varied
+        Double numOfInfectors = 0.0;
+
+
 
 
         String ini_case;
@@ -34,6 +45,9 @@ public class continue_simulation {
         ArrayList<Double> venti_list = new ArrayList<>();
         ArrayList<String> temp_coor = new ArrayList<>();
         ArrayList<Double> aver_days_pv = new ArrayList<>();
+        ArrayList<Double> percentage_ventilation = new ArrayList<>();
+        ArrayList<Double> aver_percentage_pv = new ArrayList<>();
+        ArrayList<Double> aver_percentage_distance = new ArrayList<>();
         String coor = "";
         String[] values = new String[2];
         String xy = "";
@@ -62,20 +76,29 @@ public class continue_simulation {
          * starting 100 simulations of classroom envrionments
          */
         int top_ventilation_rate = 3000; // upper bound for ventilation rate
-        Classroom.ventilation_Rate = 500;   // lower bound for ventilation rate
+        ;   // lower bound for ventilation rate
         // here, we want to see the change of average days for infecting the entire classroom, based on the varying ventilation rate
         // we run 25 simulation results for each different ventilation rate and take mean of all the values
+        System.out.println(a.ventilation_Rate);
+        FileWriter all_Percentage_Data = new FileWriter("ventilation_all_Experiments_Data.txt");
+        double control = 1; // this is the distance variable
+        Classroom.ventilation_Rate = 500;
+        int num = 1 ;
         while(Classroom.ventilation_Rate < top_ventilation_rate){
-            Classroom.ventilation_Rate = Classroom.ventilation_Rate +100;
+            //Classroom.ventilation_Rate = Classroom.ventilation_Rate +200;
+            //System.out.println(Classroom.ventilation_Rate);
+            Classroom.ventilation_Rate += 100;
             venti_list.add(Classroom.ventilation_Rate); // record the current running ventilation rate
 
-            while(ct<25) {
+            while(ct<50) {
+                numOfInfectors = 0.0;
+                num = 1;
                 while (a.current_infected < a.number_of_student) {  // while the the classroom has not been entirely affected
 
 
                     //System.out.println(a.current_infected < a.number_of_student);
                     for ( int row = 0; row < a.length ; row ++) {
-                        for (int col = 0; col < a.width ; col++) {
+                        for (int col = 0; col < a.width; col++) {
 
                             temp_quanta = 0;        // temporary quanta for storing the current student quanta
                             current_student = a.classroom[row][col];
@@ -88,7 +111,7 @@ public class continue_simulation {
                                     values = coor.split(",");
                                     x = Integer.valueOf(values[0]) - row;
                                     y = Integer.valueOf(values[1]) - col;
-                                    t_constant = trans_constant_r(x,y);
+                                    t_constant = trans_constant_r(x,y,control);
                                     temp_quanta = temp_quanta + a.quanta * t_constant;
                                 }
 
@@ -120,6 +143,14 @@ public class continue_simulation {
                     myWriter.write(",");
                     myWriter.write(String.valueOf(infectors_per_time_step));
                     myWriter.write("\n");
+                    numOfInfectors += infectors_per_time_step;
+
+                    Double percentageOfInfection = 0.0 ;
+                    if (day>= num*academicWeek && num == 1) {
+                        percentageOfInfection = numOfInfectors/(Classroom.width *(Classroom.length-2)); // depending on the current running variable, one for ventilation rate, 2 for position.
+                        percentage_ventilation.add(percentageOfInfection);
+                        num++;
+                    }
                     infectors_per_time_step = 0;
                     day++;
 
@@ -128,6 +159,7 @@ public class continue_simulation {
                     }
 
                 }
+
 
                 ct++;
 
@@ -171,17 +203,34 @@ public class continue_simulation {
                 infectors_per_time_step = 0;
                 day = 1;
 
-                //System.out.println(a.current_infected);
+
             }
 
-            //System.out.println(days.toString());
 
-            int t_sum = sum(days);
-            aver_days_pv.add((double) (t_sum/days.size()));
+            Double corner_Sum = sum(corner_days);
+            Double center_Sum = sum(center_days);
+            Double wall_Sum = sum(wall_days);
+            aver_cornerDays = corner_Sum / corner_days.size();
+            aver_centerDays = center_Sum / center_days.size();
+            aver_wallDays = wall_Sum / wall_days.size();
+            Double t_sum = sum(days);
+            double aver_day = t_sum/days.size();
+            aver_days_pv.add((aver_day));
+            Double percentage_ventilation_rate = sumD(percentage_ventilation)/percentage_ventilation.size();
+            aver_percentage_pv.add(percentage_ventilation_rate);
+            //all_Percentage_Data.write(String.valueOf(Classroom.ventilation_Rate)+ '\t');
+            for(int i   = 0 ; i < percentage_ventilation.size(); i++){
+                all_Percentage_Data.write(String.valueOf(percentage_ventilation.get(i)));
+                all_Percentage_Data.write((","));
+            }
+            all_Percentage_Data.write('\n');
+            percentage_ventilation.clear();
             days.clear();
             ct = 0;
 
         }
+        all_Percentage_Data.flush();
+        all_Percentage_Data.close();
 
 
 
@@ -197,12 +246,9 @@ public class continue_simulation {
         double sum = sum(days);
         System.out.println(aver_days_pv.toString());
         double aver_day_for_infection = sum/days.size() ;
-        double aver_day_corner = sum(corner_days)/corner_days.size();
-        double aver_day_center = sum(center_days)/center_days.size();
-        double aver_day_wall = sum(wall_days)/wall_days.size();
 
-        //aver_day_for_infection =  (sum/days.size());
-        //System.out.print(sum/days.size());
+
+
         FileWriter myday = new FileWriter("days.txt");
         int check =  0;
         for(int i = 0; i < aver_days_pv.size() ; i++) {
@@ -210,21 +256,33 @@ public class continue_simulation {
 
             myday.write(String.valueOf(aver_days_pv.get(i)) + "\t " + String.valueOf(venti_list.get(i)) +'\n');
         }
+
         myday.flush();
         myday.close();
+
+
+        FileWriter percentage = new FileWriter("Amount_of Infectors_After_1_week");
+        for(int i = 0; i < aver_days_pv.size() ; i++) {
+            percentage.write(String.valueOf(aver_percentage_pv.get(i)) + "\t " + String.valueOf(venti_list.get(i)) +'\n');
+        }
+        percentage.flush();
+        percentage.close();
 
         FileWriter output = new FileWriter("simulation_outputs.txt");
         output.write("average days for infection,");
 
         output.write(String.valueOf(aver_day_for_infection) + "\n");
 
-        output.write("center cases,");
-        //System.out.println(check);
-        output.write(String.valueOf(centercase) +"," + String.valueOf(aver_day_center) + "\n");
         output.write("corner cases,");
-        output.write(String.valueOf(cornercase) +"," + String.valueOf(aver_day_corner) + "\n");
+        System.out.println(days);
+        System.out.println(corner_days);
+        System.out.println(center_days);
+        System.out.println(wall_days);
+        output.write(String.valueOf(cornercase) +"," + String.valueOf(aver_cornerDays) + "\n");
+        output.write("center cases,");
+        output.write(String.valueOf(centercase) +"," + String.valueOf(aver_centerDays) + "\n");
         output.write("wall cases," );
-        output.write(String.valueOf(wallcase) +"," + String.valueOf(aver_day_wall) + "\n");
+        output.write(String.valueOf(wallcase) +"," + String.valueOf(aver_wallDays) + "\n");
 
         output.close();
 
@@ -249,8 +307,8 @@ public class continue_simulation {
      * @param y  coordinates diff
      * @return transmission coefficient
      */
-    public static double trans_constant_r (int x , int y) {
-        return 1/(Math.pow(x, 2)+ Math.pow(y, 2));  // assuming we do not need to consider the influence of the Z axis
+    public static double trans_constant_r (int x , int y, double control) {
+        return 1/(Math.pow(x*control, 2)+ Math.pow(y*control, 2));  // assuming we do not need to consider the influence of the Z axis
 
     }
 
@@ -285,9 +343,16 @@ public class continue_simulation {
      * @param x
      * @return
      */
-    public static int sum (ArrayList<Integer> x) {
-        int sum = 0;
+    public static Double sum (ArrayList<Integer> x) {
+        Double sum = 0.0;
         for (int c : x) {
+            sum += c;
+        }
+        return sum;
+    }
+    public static Double sumD (ArrayList<Double> x) {
+        Double sum = 0.0;
+        for (Double c : x) {
             sum += c;
         }
         return sum;
